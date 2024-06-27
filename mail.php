a202b0da81a57efc8aecb0aba8f9b48036842311
muhammed5rafi <muhammed5rafi>
2024-06-27 15:52:55 +0530

    mail changes

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email= $_POST['email'];
 $subject= $_POST['subject'];
 $message= $_POST['message'];
-$to = "sharafumthouse@gmail.com";
+$to = "muhammed5rafi@gmail.com";
 $subject = "Mail From personal_website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject =" . $subject . "\r\n Message =" . $message;
 $headers = "From: noreply@personal.com" . "\r\n" .
